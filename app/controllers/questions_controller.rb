f7ea364b500cf5e6814b60c3a5e080464b20a28d
Mac <mac@Macs-MacBook-Pro.local>
2020-02-07 11:59:26 +1300

    Questions added to Studies + rendering

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,12 @@
 class QuestionsController < ApplicationController
+  def create
+    @study = Study.find(params[:study_id])
+    @question = @study.questions.create(question_params)
+    redirect_to study_path(@study)
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:question)
+  end
 end
