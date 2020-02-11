c85d49a4d90b15b4e7408f700c9402869aa532d7
Mac <mac@Macs-MacBook-Pro.local>
2020-02-11 13:48:29 +1300

    Styling + Testing scripts

--- test/models/study_test.rb
+++ test/models/study_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class StudyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # test 'valid study' do
+  #   study = Study.new(study_name: 'New Study Name Woooo')
+  #   assert study.valid?
   # end
+
+  # test 'invalid without study name' do
+  #   study = Study.new(study_name: 'sdfghjkvcx')
+  #   assert study.valid?, 'study is not valid without a name'
+  #   assert_not_nil study.errors[:study_name], 'no validation error for name present'
+  # end
+  def setup
+    @study = Study.new(study_name: 'sdfghgfds')
+  end
+
+  test 'valid study' do
+    assert @study.valid?
+  end
+
+  test 'invalid without study name' do
+    @study.study_name = nil
+    refute @study.valid?, 'saved study without a name'
+    assert_not_nil @study.errors[:study_name], 'no validation error for study name present'
+  end
+
 end
