899175401840ce2ed2fea214ed8ba0280c39d405
Katie French <katie.french@optimalworkshop.com>
2020-03-04 10:38:54 +1300

    Changed colour of remaining chars from white to red when in negatives

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
     var text_max = 100;
     $('#textarea_feedback').html(text_max + ' characters remaining');
-
     $('#textarea').keyup(function() {
         var text_length = $('#textarea').val().length;
         var text_remaining = text_max - text_length;
-
+        if(text_remaining >= 0 ){
+        } else {
+            document.getElementById('textarea_feedback').style.color = '#FC427B';
+        }
         $('#textarea_feedback').html(text_remaining + ' characters remaining');
     });
 });
